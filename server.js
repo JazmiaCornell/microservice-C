82836fa7da07479b8b5644b94f87ddd2922afc7d
Jazmia Cornell <jazmiadc@gmail.com>
2025-05-31 22:22:47 -0400

    implement microservice-C, process Stripe payments

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // using Stripe for donations/payments
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+// sends payment request to stripe for processing
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount, email } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100), // amount in cents
+      currency: "usd",
+      receipt_email: email,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "PaymentIntent creation failed" });
+  }
+});
+
+// listener
+app.listen(8089, () => {
+  console.log("server listening on port 8089, microservice-C");
+});
